90c1661ce3400d466d3886a844c21d18a11d42b3
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-08-15 20:03:47 -0400

    Campos para criar tabela

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
     public titulo = 'Alunos';
 
     public alunos = [
-        { nome: 'Amanda' },
-        { nome: 'Fernando' },
-        { nome: 'Gabriela' },
-        { nome: 'Marquinhos' },
-        { nome: 'Nayra' },
-        { nome: 'Older' },
-        { nome: 'Wilson' }
+        { id: 1, nome: 'Amanda', sobrenome: 'Kent', telefone: 321321, },
+        { id: 2, nome: 'Fernando', sobrenome: 'Augusto', telefone: 159159, },
+        { id: 3, nome: 'Gabriela', sobrenome: 'Tedezco', telefone: 156156, },
+        { id: 4, nome: 'Marquinhos', sobrenome: 'Relampago', telefone: 741741, },
+        { id: 5, nome: 'Nayra', sobrenome: 'Nira', telefone: 954954, },
+        { id: 6, nome: 'Older', sobrenome: 'Young', telefone: 658658, },
+        { id: 7, nome: 'Wilson', sobrenome: 'Afonso', telefone: 656565, }
     ]
 
     constructor() { }
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -10,13 +10,13 @@ export class ProfessoresComponent implements OnInit {
     public titulo = "Professores"
 
     public professores = [
-        { nome: "Arlindo" },
-        { nome: "Abigaiu" },
-        { nome: "Clotilde" },
-        { nome: "Fabiano" },
-        { nome: "Mariafalda" },
-        { nome: "Paulinea" },
-        { nome: "Sancho Pança" },
+        { id: 1, nome: "Arlindo", disciplina: 'Matemática' },
+        { id: 2, nome: "Abigaiu", disciplina: 'Física' },
+        { id: 3, nome: "Clotilde", disciplina: 'Português' },
+        { id: 4, nome: "Fabiano", disciplina: 'Inglês' },
+        { id: 5, nome: "Mariafalda", disciplina: 'Espanhol' },
+        { id: 6, nome: "Paulinea", disciplina: 'Filosofia' },
+        { id: 7, nome: "Sancho Pança", disciplina: 'História' },
     ]
 
     constructor() { }
